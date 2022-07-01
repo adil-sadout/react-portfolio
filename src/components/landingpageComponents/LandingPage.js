4182921afea21777aac991903797e7454eaf1233
Adil Sadout <adelsadout@hotmail.com>
2022-07-01 12:49:36 +0100

    changed content on landing page

--- src/components/landingpageComponents/LandingPage.js
+++ src/components/landingpageComponents/LandingPage.js
@@ -1,5 +1,6 @@
 import {NavLink} from "react-router-dom"
 
+
 export default function LandingPage(){
     return(
         <>
@@ -8,14 +9,11 @@ export default function LandingPage(){
             
             
             <div className="landingPage-content">
-                <p className="display-5">Hello, I'm Adil, Front End Engineer, And Your Next Website Creator :)</p>
-                <p className="lead">You will find all you need to know about my previous experiences, projects, and contact information by checking the links below</p>
+                <p className="display-1">Hello, I'm Adil</p>
+                <p className="display-6">A Web Developer Specialized In Front End Technologies</p>
+                
             </div>
-            <div className="landingPage-buttons">
-                <NavLink className="btn btn-warning m-1  btn-lg" to="/projects" >Projects</NavLink>
-                <a  className="btn btn-warning m-1  btn-lg" href="https://github.com/adil-sadout" rel="noreferrer" target="_blank">Github</a>
-                <NavLink className="btn btn-warning m-1  btn-lg" to="/contact" >Contact</NavLink>
-            </div>
+            
             
             
             
